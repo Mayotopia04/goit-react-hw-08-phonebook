c03d8f560c340401ef66295ee26598ad9ac11144
Mayotopia <134679840+Mayotopia04@users.noreply.github.com>
2024-03-29 11:28:36 +0800

    img, footer
    remove unnecessary  images, footer adjust but not done yet

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,16 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, BottomNavigation } from "@mui/material";
 // import css from "./Footer.module.css";
 
 export const Footer = () => {
     return (
         <>
-            <Box sx={{backgroundColor: "#62487bae", position: "static"}}>
+            <BottomNavigation sx={{backgroundColor: "#62487bae"}}>
+            <Box sx={{position: "static"}}>
                 
                 <Typography sx={{ color: "white", justifyContent: "center", height: "50px", alignItems: "center", display: "flex"}}>&copy; {new Date().getFullYear()} |  Developed by &nbsp;<a style={{fontSize: "bold", color: "lightgreen"}} href="mayotopia04.github.io/goit-react-hw-08-phonebook">mayotopia04</a>&nbsp; of GoIT Global🧡
                 </Typography>
-            </Box>
+                </Box>
+            </BottomNavigation>
         </>
     );
 };
